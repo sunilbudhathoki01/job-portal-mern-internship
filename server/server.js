ff59a4aa04257359146e9df23f32b26df057226d
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2026-01-09 21:17:29 +0545

    modified server.js and added and call coonectcloudinary function

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import { connectDb } from "./config/db.js";
 import webhookRouter from "./routes/webhookRoutes.js";
 import companyRouter from "./routes/companyRoutes.js";
+import { connectCloudinary } from "./config/cloudinary.js";
 
 // initialize expree
 const app = express();
@@ -12,8 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// call mongodb connection function
+// call mongodb connection function and cloudinary config
 await connectDb();
+await connectCloudinary();
 
 // routes
 app.use("/webhooks", webhookRouter);
